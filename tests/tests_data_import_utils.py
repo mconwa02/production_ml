498e1f6c8e0c8a116264f2e59eeed57d2fd07d50
mconwa02 <mconwa02@dcs.bbk.ac.uk>
2022-12-28 17:39:20 +0000

    adding tests

--- tests/tests_data_import_utils.py
+++ tests/tests_data_import_utils.py
@@ -8,6 +8,8 @@ from components.data_import.utils import read_csv_write_parquet
 def test_read_csv_write_parquet():
     test_path = Path.cwd().joinpath("data")
     read_csv_write_parquet(test_path, "cust_data")
-    expected = pd.DataFrame(data={"customer": ["A", "B", "C"], "products": [11, 1, 13]})
+    expected = pd.DataFrame(
+        data={"customer": ["A", "B", "C"], "products": [11, 1, 13]}
+    )
     actual = pd.read_parquet(test_path.joinpath("cust_data.pq"))
     pd.testing.assert_frame_equal(actual, expected)
