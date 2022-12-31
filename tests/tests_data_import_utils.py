ccb050649cc1efed9a87b35f6d6996ff8f888b6a
mconwa02 <mconwa02@dcs.bbk.ac.uk>
2022-12-31 17:29:24 +0000

    adding stuff

--- tests/tests_data_import_utils.py
+++ tests/tests_data_import_utils.py
@@ -11,5 +11,4 @@ def test_read_csv_write_parquet():
     expected = pd.DataFrame(
         data={"customer": ["A", "B", "C"], "products": [11, 1, 13]}
     )
-    # actual = pd.read_parquet(test_path.joinpath("cust_data.pq"))
     pd.testing.assert_frame_equal(actual, expected)
